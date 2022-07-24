aa3cab2ffc0ec14e0e87afc288fa57945c29b633
wilderedin5 <wildnefalem5@gmail.com>
2022-07-24 13:26:28 +0300

    include montserrat font, edit login and registration templates, add files for mixins, fonts, pages style.

---
+++ src/components/button/index.js
@@ -0,0 +1,6 @@
+import Handlebars from 'handlebars';
+import template from "./template.hbs";
+
+Handlebars.registerPartial('button', template)
+
+export default template
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.scss";
+
+import template from './index.hbs';
+import page404 from './pages/404';
+import input from './components/input'
+import field from './components/field'
+import button from './components/button'
+
+document.getElementById('root').innerHTML = template();
